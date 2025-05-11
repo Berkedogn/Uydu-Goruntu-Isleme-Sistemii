5c51f9cab96a2c5111d5aa2cab6a523841a95b2a
Selman1071 <Dkselman04@gmail.com>
2025-05-11 22:01:24 +0300

    Update Forest.cs
    Signed-off-by: Selman1071 <Dkselman04@gmail.com>

--- Forest.cs
+++ Forest.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+// Models/Forest.cs
+using System.ComponentModel.DataAnnotations;
 
 namespace SatelliteImageExplorer.Models
 {
@@ -7,14 +8,20 @@ namespace SatelliteImageExplorer.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Orman adı zorunludur.")]
-        [MaxLength(100)]
+        [StringLength(100, ErrorMessage = "En fazla 100 karakter olabilir.")]
+        [Display(Name = "Orman Adı")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Açıklama zorunludur.")]
+        [Display(Name = "Açıklama")]
         public string Description { get; set; }
 
+        [Url(ErrorMessage = "Geçerli bir URL girin.")]
+        [Display(Name = "Resim URL")]
         public string ImagePath { get; set; }
 
+        [Required(ErrorMessage = "Şehir bilgisi zorunludur.")]
+        [Display(Name = "Şehir")]
         public string City { get; set; }
     }
 }
