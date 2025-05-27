63a0ba9d5d421446d462517dd4e02c86464e0f9b
Berkedogn <139199335+Berkedogn@users.noreply.github.com>
2025-05-27 16:29:34 +0300

    Update Beach.cs
    Signed-off-by: Berkedogn <139199335+Berkedogn@users.noreply.github.com>

--- Beach.cs
+++ Beach.cs
@@ -1,23 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
-namespace SatelliteImageExplorer.Models
+namespace UyduGoruntu.Models
 {
     public class Beach
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Sahil adı zorunludur.")]
-        [StringLength(100, ErrorMessage = "Sahil adı en fazla 100 karakter olabilir.")]
-        [Display(Name = "Sahil Adı")]
+        [Required(ErrorMessage = "Plaj adı zorunludur.")]
+        [StringLength(100)]
         public string Name { get; set; }
 
-        [Display(Name = "Şehir")]
-        public string? City { get; set; }
+        [Display(Name = "Konum")]
+        public string? Location { get; set; }
 
         [Display(Name = "Açıklama")]
         public string? Description { get; set; }
 
-        [Display(Name = "Uydu Görüntüsü URL")]
+        [Display(Name = "Görsel URL")]
         public string? ImageUrl { get; set; }
     }
 }
